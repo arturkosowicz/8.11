0ca14c0adc9a98a302ac1793556286bec29f9ec3
Artur K <artur@gmail.com>
2017-03-23 20:36:47 +0100

    ver.01

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function click_function() {
+    var li = document.getElementsByTagName('li').length;
+    list.innerHTML += '<li>item ' + li + '</li>';
+}
+
+var add = document.getElementById('addElem');
+var list = document.getElementById('list');
+
+add.addEventListener('click', clickFunction);
